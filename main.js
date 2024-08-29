e6aa7c7d37c4b98f75eafb25b12dabc3728f96dc
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2024-08-29 05:11:42 -0300

    Add navbar internal links functionality with goTo()

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function goTo(element_id) {  
+    const REM_IN_PIXELS = parseFloat(getComputedStyle(document.documentElement).fontSize)
+    const TARGET = element_id === "top" ? 0 : document.getElementById(element_id).offsetTop - (REM_IN_PIXELS * 9)
+
+    window.scrollTo({
+        top: TARGET,
+        behavior: 'smooth'
+    })
+}
+
 function openNavbarDrawer() {
     let x = document.getElementById("navbarLinks")
     if (x.style.display === "flex") {
